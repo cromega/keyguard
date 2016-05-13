86b0a53e540e33094cbc6d4a1dda6339bd67c3a9
Bence Monus <crome@moronia.hu>
2016-05-13 10:24:50 +0100

    improve loader

--- loader.sh
+++ loader.sh
@@ -2,10 +2,11 @@ echo -n "OTP: "
 read password < /dev/tty
 
 keyfile=$(mktemp)
-curl -f -s -u "cromega:$password" "{{ .Url }}" > "$keyfile"
+trap "rm -rf "$keyfile"" EXIT
+
+curl -k -f -s -u "cromega:$password" "{{ .Url }}" > "$keyfile"
 ret=$?
 if [ $ret -eq 0 ]; then
-  trap "rm -rf "$keyfile"" EXIT
   ssh-add -t 32400 "$keyfile"
 else
   echo "something went wrong."
