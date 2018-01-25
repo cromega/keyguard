831e718a0fda9c1b6be685442ffdabc4cfd11896
Bence Monus <crome@moronia.hu>
2018-01-25 00:14:19 +0000

    use generic username

--- loader.sh
+++ loader.sh
@@ -9,7 +9,7 @@ fi
 keyfile=$($mktemp)
 trap "rm -rf $keyfile" EXIT
 
-curl -f -s -u "cromega:$password" "{{ .URL }}" > "$keyfile"
+curl -f -s -u "keyguard:$password" "{{ .URL }}" > "$keyfile"
 if [ $? ]; then
   ssh-add -t 32400 "$keyfile"
 else
