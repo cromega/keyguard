853e8529c6c27323c238419964d641bd53db879e
Bence Monus <crome@moronia.hu>
2020-03-01 20:44:38 +0000

    kubectl create fails if resource exists so manage yml with apply

--- ci/prepare_deployment.sh
+++ ci/prepare_deployment.sh
@@ -7,7 +7,10 @@ buildroot=$PWD
 cd code/ci/k8s
 envsubst < deployment.yml > temp.yml
 mv temp.yml deployment.yml
+
+export PRIVATE_KEY_BASE64="$(echo "$PRIVATE_KEY" | base64)"
+envsubst < secret.yml > temp.yml
+mv temp.yml secret.yml
 cd $buildroot
 
 cp code/ci/k8s/* deployment/
-echo "$PRIVATE_KEY" > deployment/private_key
