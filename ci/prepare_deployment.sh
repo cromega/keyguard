416a2fda1518db681c75fdc50358d724418fb69d
Bence Monus <crome@moronia.hu>
2020-03-01 20:52:14 +0000

    don't mangle base64

--- ci/prepare_deployment.sh
+++ ci/prepare_deployment.sh
@@ -8,7 +8,6 @@ cd code/ci/k8s
 envsubst < deployment.yml > temp.yml
 mv temp.yml deployment.yml
 
-export PRIVATE_KEY_BASE64="$(echo "$PRIVATE_KEY" | base64)"
 envsubst < secret.yml > temp.yml
 mv temp.yml secret.yml
 cd $buildroot
