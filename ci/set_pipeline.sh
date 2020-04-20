f917f45a03777672a0a76976b9c56f23ad5674f2
Bence Monus <crome@moronia.hu>
2020-04-20 22:33:17 +0200

    don't deploy the build, it's not a repo concern

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e
 
-fly -t devbox set-pipeline -p keyguard -c ci/pipeline.yml \
-  -v "dockerhub-password=$(lpass show --password docker.com)" \
+lpass status || { echo "log in to lastpass first."; exit 1; }
+
+fly -t ci set-pipeline -p keyguard -c ci/pipeline.yml \
   -v "deploy-key=$(lpass show keyguard-deploy-key --field='Private Key')" \
-  -v "ci-access-key-id=$(lpass show ci-user-access-key --field=access-key-id)" \
-  -v "ci-secret-access-key=$(lpass show ci-user-access-key --field=secret-access-key)" \
-  -v "yubico-client-id=$(lpass show yubico --field=client-id)" \
-  -v "yubico-api-key=$(lpass show yubico --field=api-key)" \
-  -v "private-key=$(lpass show devkey --field='Private Key' | base64 -w0)"
+  -v "dockerhub-password=$(lpass show --password docker.com)"
