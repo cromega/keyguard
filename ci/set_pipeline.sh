2ec2ac5670918b8fbf87eb7552b3f39566c9583b
Bence Monus <crome@moronia.hu>
2020-09-10 22:33:26 +0200

    deploy with helm for less pain

--- ci/set_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-lpass status || { echo "log in to lastpass first."; exit 1; }
-
-fly -t ci set-pipeline -p keyguard -c ci/pipeline.yml \
-  -v "deploy-key=$(lpass show keyguard-deploy-key --field='Private Key')" \
-  -v "dockerhub-password=$(lpass show --password docker.com)"
